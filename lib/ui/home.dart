9ac68a25d58c8f5c528609ada0359fa110f54430
Ajit Pratap Singh <ajit.singh3@olacabs.com>
2018-12-10 20:23:22 +0530

    Adding Image Asset & Raised Button

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -13,8 +13,7 @@ class Home extends StatelessWidget {
           Row(
             children: <Widget>[
               Expanded(
-                  child: Text(
-                      "Margherita",
+                  child: Text("Margherita",
                       textDirection: TextDirection.ltr,
                       style: TextStyle(
                           color: Colors.white,
@@ -56,9 +55,48 @@ class Home extends StatelessWidget {
                         fontWeight: FontWeight.w400)),
               )
             ],
-          )
+          ),
+          PizzaImageWidget(),
+          OrderButton(),
         ],
       )),
     );
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = AssetImage('images/pizza1.jpg');
+    Image image = Image(image: pizzaAsset, width: 400.0, height: 400.0);
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Order Pizza"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+    return button;
+  }
+
+  void order(BuildContext con) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for ordering!!"),
+    );
+    showDialog(context: con, builder: (BuildContext context) => alert);
   }
 }
