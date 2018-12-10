264d4f97aed75dfad64b089bfc7d6f4f716084c2
Ajit Pratap Singh <ajit.singh3@olacabs.com>
2018-12-10 20:51:38 +0530

    added polish

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,7 +6,7 @@ class Home extends StatelessWidget {
     return Container(
       alignment: Alignment.center,
       color: Colors.deepOrangeAccent,
-      padding: EdgeInsets.all(50.0),
+      padding: EdgeInsets.only(top:25.0,bottom: 16.0,left: 16.0, right: 16.0),
       child: Center(
           child: SingleChildScrollView(
             child: Column(
@@ -14,46 +14,58 @@ class Home extends StatelessWidget {
             Row(
               children: <Widget>[
                 Expanded(
-                    child: Text("Margherita",
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Margherita",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              decoration: TextDecoration.none,
+                              fontFamily: 'Nunito',
+                              fontWeight: FontWeight.w400)),
+                    )),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Tomato, Mozarella cheese, Basil",
                         textDirection: TextDirection.ltr,
                         style: TextStyle(
                             color: Colors.white,
-                            fontSize: 30,
+                            fontSize: 20,
                             decoration: TextDecoration.none,
                             fontFamily: 'Nunito',
-                            fontWeight: FontWeight.w400))),
-                Expanded(
-                  child: Text("Tomato, Mozarella cheese, Basil",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          decoration: TextDecoration.none,
-                          fontFamily: 'Nunito',
-                          fontWeight: FontWeight.w400)),
+                            fontWeight: FontWeight.w400)),
+                  ),
                 )
               ],
             ),
             Row(
               children: <Widget>[
                 Expanded(
-                    child: Text("Marinara",
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Marinara",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              decoration: TextDecoration.none,
+                              fontFamily: 'Nunito',
+                              fontWeight: FontWeight.w400)),
+                    )),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Tomato, Garlic",
                         textDirection: TextDirection.ltr,
                         style: TextStyle(
                             color: Colors.white,
-                            fontSize: 30,
+                            fontSize: 20,
                             decoration: TextDecoration.none,
                             fontFamily: 'Nunito',
-                            fontWeight: FontWeight.w400))),
-                Expanded(
-                  child: Text("Tomato, Garlic",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          decoration: TextDecoration.none,
-                          fontFamily: 'Nunito',
-                          fontWeight: FontWeight.w400)),
+                            fontWeight: FontWeight.w400)),
+                  ),
                 )
               ],
             ),
@@ -66,22 +78,24 @@ class Home extends StatelessWidget {
   }
 }
 
+// Custom Widget to display Image
 class PizzaImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage pizzaAsset = AssetImage('images/pizza1.jpg');
-    Image image = Image(image: pizzaAsset, width: 400.0, height: 400.0);
+    Image image = Image(image: pizzaAsset,height: 300.0,width: 350.0);
     return Container(
-      child: image,
+      child: image
     );
   }
 }
 
+//Custom Widget for a Raised button
 class OrderButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var button = Container(
-      margin: EdgeInsets.only(top: 50.0),
+      margin: EdgeInsets.only(top: 16.0),
       child: RaisedButton(
         child: Text("Order Pizza"),
         color: Colors.lightGreen,
@@ -94,6 +108,7 @@ class OrderButton extends StatelessWidget {
     return button;
   }
 
+  //function to display an alert dialog
   void order(BuildContext con) {
     var alert = AlertDialog(
       title: Text("Order Completed"),
